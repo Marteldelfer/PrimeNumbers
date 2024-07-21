3dfa050e0ba44b20e5981026b4dd9f0288baa6b6
Murillo Ferraz <marteldelfer@gmail.com>
2024-07-21 20:14:06 -0300

    Adding extra functions to generate prime numbers
    Adds the function first_n_primes, witch returns a list of the first n prime numbers.
    Also adds the function find_nth_prime, witch returns the nth prime number.

--- primos.py
+++ primos.py
@@ -1,4 +1,22 @@
-def primos(n: int):
+def find_nth_prime(n: int) -> int:
+    lista: list = [2]
+    counter: int = 1
+    a: int = 3
+
+    while counter <= n:
+        a += 1
+        divisivel: bool = False
+        for i in lista:
+            if a % i == 0:
+                divisivel = True
+                break
+        if not divisivel:
+            counter += 1
+            lista.append(a)
+    
+    return lista[counter - 1]
+
+def primes_to_n(n: int) -> list:
     lista = [2]
 
     for a in range(3, n, 2):
@@ -10,9 +28,37 @@ def primos(n: int):
         if divisível == False:
             lista.append(a)
     return lista
-    
-if __name__ == '__main__':
+
+def first_n_primes(n: int) -> list:
+    lista = [2]
+    counter: int = 1
+    a: int = 3
+
+    while counter <= n:
+        a += 1
+        divisivel: bool = False
+        for i in lista:
+            if a % i == 0:
+                divisivel = True
+                break
+        if not divisivel:
+            counter += 1
+            lista.append(a)
+
+    return lista
+
+def main() -> None:
     from time import time
     start = time()
-    print(primos(100000))
+    print(primes_to_n(50_000))
     print(time() - start)
+    start = time()
+    print(first_n_primes(5_000))
+    print(time() - start)
+    start = time()
+    print(find_nth_prime(5_000))
+    print(time() - start)
+    
+
+if __name__ == '__main__':
+    main()
