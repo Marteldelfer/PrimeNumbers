7d90a7ebab28b1110d1f1406f4d881df7251424c
Murillo Ferraz <marteldelfer@gmail.com>
2024-07-22 22:14:25 -0300

    Update primos.py

--- primos.py
+++ primos.py
@@ -1,18 +1,1 @@
-def primos(n: int) -> list:
-    lista = [2]
 
-    for a in range(3, n, 2):
-        divisível: bool = False
-        for b in lista:
-            if a%b == 0:
-                divisível = True
-                break
-        if divisível == False:
-            lista.append(a)
-    return lista
-    
-if __name__ == '__main__':
-    from time import time
-    start = time()
-    print(primos(100000))
-    print(time() - start)
